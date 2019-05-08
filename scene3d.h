878b62f5801bca2cc2125a6d141f16f2391e75e9
magistrkoljan <olawed@gmail.com>
2019-05-08 16:47:33 +0300

    + Scale

--- scene3d.h
+++ scene3d.h
@@ -13,6 +13,17 @@ class Scene3D : public QGLWidget
     GLfloat zTra;
     GLfloat nSca;
 
+  //  QPoint ptrMousePosition;
+
+
+    void scale_plus();
+    void scale_minus();
+    void rotate_up();
+    void rotate_down();
+    void rotate_left();
+    void rotate_right();
+    void translate_down();
+    void translate_up();
     void drawAxis();
     void defaultScene();
     void my_getArrays();
@@ -22,6 +33,10 @@ protected:
     void initializeGL();
     void resizeGL(int nWidth, int nHeight);
     void paintGL();
+    //void mousePressEvent(QMouseEvent* pe);
+    //void mouseMoveEvent(QMouseEvent* pe);
+   // void mouseReleaseEvent(QMouseEvent* pe);
+    void keyPressEvent(QKeyEvent* pe);
 
    public:
       Scene3D(QWidget* parent = 0);
